7c06cc770d459de213c518c8b9accbaaf8abd328
gigavivek <2302182@dhempecollege.edu.in>
2025-08-13 11:03:17 +0530

    first code

---
+++ hello.py
@@ -0,0 +1,12 @@
+msg="Hello world"
+print(type(msg))
+msg=5
+print(msg)
+print(type(msg))
+x=str(msg)
+print(x)
+print(type(x))
+   x,y=["Apple","arrenges","drenam"]
+   print(y+x+z)
+   if(5>4)
+   print("5 in greater than 4")
